4f027b3431d6313e4e584d83640872bc77e1795a
052hide <hidetoshi1990@gmail.com>
2021-08-15 00:51:25 +0900

    add eslint disable

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { alias, aliasJest, configPaths } = require('react-app-rewire-alias')
 
 const aliasMap = configPaths('./tsconfig.paths.json')
