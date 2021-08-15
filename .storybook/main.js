29470bca56ec0d4484839558823cdbe328ae89a3
052hide <hidetoshi1990@gmail.com>
2021-08-15 12:11:56 +0900

    add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+  ],
+  webpackFinal: async (config) => {
+    config.resolve.alias['~'] = path.resolve(__dirname, '../src/');
+    return config;
+  },
+}
